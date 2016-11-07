8a1cbc54b9c512922607cc1ec78b90372e2a8a81
borja sahuquillo ferrer <borja sahuquillo ferrer>
2016-11-07 19:01:43 +0100

    Unidad 6 Finalizada

--- Unidad6/Ejemplo12/src/ejemplo12/Ejemplo12.java
+++ Unidad6/Ejemplo12/src/ejemplo12/Ejemplo12.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejemplo12;
 
 import javax.validation.ConstraintViolation;
@@ -13,17 +8,9 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 
-/**
- *
- * @author borja
- */
 public class Ejemplo12 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-
         SessionFactory sessionFactory;
 
         Configuration configuration = new Configuration();
@@ -31,26 +18,146 @@ public class Ejemplo12 {
         ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
         sessionFactory = configuration.buildSessionFactory(serviceRegistry);
 
-        Profesor profesor = new Profesor( null, null, "Gomez");
-        //--Session--
-        Session session = sessionFactory.openSession();
-        
-        try{
-            session.beginTransaction();
-            session.save(profesor);
-            session.getTransaction().commit();
-            
-        }catch(ConstraintViolationException cve){
-            session.getTransaction().rollback();
-            System.out.println("No se ha podido insertar el profesor por los siguientes errores: ");
-            for (ConstraintViolation constraintViolation: cve.getConstraintViolations()){
-                System.out.println("En el campo '" + constraintViolation.getPropertyPath()+ "':" + constraintViolation.getMessage());
+        {
+            System.out.println("----------- Las primeras validaciones -----------");
+            Session session = sessionFactory.openSession();
+            Profesor profesor = new Profesor("ca", null, "Gomez");
+
+            try {
+
+                session.beginTransaction();
+
+                session.save(profesor);
+
+                session.getTransaction().commit();
+            } catch (ConstraintViolationException cve) {
+                session.getTransaction().rollback();
+                System.out.println("No se ha podido insertar el profesor");
             }
+            session.close();
+
         }
 
-//        
-        session.close();
-    
-    }
+        {
+            System.out.println("----------- Mensajes de error -----------");
+            Session session = sessionFactory.openSession();
+            Profesor profesor = new Profesor(null, null, "Gomez");
 
+            try {
+                session.beginTransaction();
+
+                session.save(profesor);
+
+                session.getTransaction().commit();
+            } catch (ConstraintViolationException cve) {
+                session.getTransaction().rollback();
+                System.out.println("No se ha podido insertar el profesor debido a los siguientes errores:");
+                for (ConstraintViolation constraintViolation : cve.getConstraintViolations()) {
+                    System.out.println("En el campo '" + constraintViolation.getPropertyPath() + "':" + constraintViolation.getMessage());
+                }
+            }
+            session.close();
+        }
+
+        {
+            System.out.println("----------- Expresion en JavaScript -----------");
+            Session session = sessionFactory.openSession();
+            Usuario usuario = new Usuario("luis", "Luis", "Ramirez", "Cano", "1234567", "12345678");
+
+            try {
+                session.beginTransaction();
+
+                session.save(usuario);
+
+                session.getTransaction().commit();
+            } catch (javax.validation.ConstraintViolationException cve) {
+                session.getTransaction().rollback();
+                System.out.println("No se ha podido insertar el profesor debido a los siguientes errores:");
+                for (ConstraintViolation constraintViolation : cve.getConstraintViolations()) {
+                    System.out.println("En el campo '" + constraintViolation.getPropertyPath() + "':" + constraintViolation.getMessage());
+                }
+            }
+
+            session.close();
+        }
+
+        {
+            System.out.println("----------- Validación por métodos -----------");
+            Session session = sessionFactory.openSession();
+            Usuario usuario = new Usuario("luisramirez", "Luis", "Ramirez", "Cano", "luisramirez", "luisramirez");
+
+            try {
+                session.beginTransaction();
+
+                session.save(usuario);
+
+                session.getTransaction().commit();
+            } catch (javax.validation.ConstraintViolationException cve) {
+                session.getTransaction().rollback();
+                System.out.println("No se ha podido insertar el profesor debido a los siguientes errores:");
+                for (ConstraintViolation constraintViolation : cve.getConstraintViolations()) {
+                    System.out.println("En el campo '" + constraintViolation.getPropertyPath() + "':" + constraintViolation.getMessage());
+                }
+            }
+
+            session.close();
+        }
+        
+        {
+            System.out.println("----------- Añadir usuario lramirez -----------");
+            Session session = sessionFactory.openSession();
+            Usuario usuario = new Usuario("lramirez","Luis","Ramirez","Cano","12345678","12345678");
+
+            try {
+                session.beginTransaction();
+
+                session.save(usuario);
+
+                session.getTransaction().commit();
+            } catch (javax.validation.ConstraintViolationException cve) {
+                session.getTransaction().rollback();
+                System.out.println("No se ha podido insertar el profesor debido a los siguientes errores:");
+                for (ConstraintViolation constraintViolation : cve.getConstraintViolations()) {
+                    System.out.println("En el campo '" + constraintViolation.getPropertyPath() + "':" + constraintViolation.getMessage());
+                }                
+            } catch (org.hibernate.exception.ConstraintViolationException cve) {
+                session.getTransaction().rollback();
+                System.out.println("No se ha podido insertar el profesor debido al siguiente error:");
+                System.out.println("El valor ya existe."+cve.getLocalizedMessage());
+            }
+            
+            session.close();
+        }        
+        
+        
+        
+        
+         {
+            System.out.println("----------- Volver a añadir usuario lramirez -----------");
+            Session session = sessionFactory.openSession();
+            Usuario usuario = new Usuario("lramirez","Luis","Ramirez","Cano","12345678","12345678");
+
+            try {
+                session.beginTransaction();
+
+                session.save(usuario);
+
+                session.getTransaction().commit();
+            } catch (javax.validation.ConstraintViolationException cve) {
+                session.getTransaction().rollback();
+                System.out.println("No se ha podido insertar el profesor debido a los siguientes errores:");
+                for (ConstraintViolation constraintViolation : cve.getConstraintViolations()) {
+                    System.out.println("En el campo '" + constraintViolation.getPropertyPath() + "':" + constraintViolation.getMessage());
+                }                
+            } catch (org.hibernate.exception.ConstraintViolationException cve) {
+                session.getTransaction().rollback();
+                System.out.println("No se ha podido insertar el profesor debido al siguiente error:");
+                System.out.println("El valor ya existe."+cve.getLocalizedMessage());
+            }
+            
+            session.close();
+        }        
+
+        sessionFactory.close();
+    }
 }
--- Unidad6/Ejemplo12/src/ejemplo12/Profesor.java
+++ Unidad6/Ejemplo12/src/ejemplo12/Profesor.java
@@ -1,91 +1,90 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejemplo12;
 
 import java.io.Serializable;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-/**
- *
- * @author borja
- */
-@Entity
-@Table(name = "Profesor")
-public class Profesor implements Serializable {
-
-    @Id
-    @Column(name="Id")
-    @GeneratedValue( generator = "generador_propietario_hibernate_increment" )
-    @org.hibernate.annotations.GenericGenerator(
-        name = "generador_propietario_hibernate_increment",
-        strategy = "increment"
+public class Profesor implements Serializable  {
     
-    )
     private int id;
-
-    @Column(name = "nombre")
     @NotNull
-    @Size(min = 3, max = 50)
+    @Size(min = 3, max = 50)    
     private String nombre;
-    
-    @NotNull(message="No puede estar vacio")
+    @NotNull(message="No puede estar vacío")    
     @Size(min = 3, max = 50)
-    @Column(name = "ape1")
     private String ape1;
-    
-    @NotNull
-    @Size(min = 3, max = 50)
-    @Column(name = "ape2")
+    @Size(min = 3, max = 50)    
     private String ape2;
 
-    public Profesor() {
+    public Profesor(){ 
+        
     }
-
-    public Profesor( String nombre, String ape1, String ape2) {       
+    
+    public Profesor(String nombre, String ape1, String ape2) {
         this.nombre = nombre;
         this.ape1 = ape1;
         this.ape2 = ape2;
+    }    
+
+    @Override
+    public String toString() {
+        return getId() + "-" + getNombre() + " " + getApe1() + " " + getApe2();
     }
 
+    /**
+     * @return the id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * @param id the id to set
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * @return the nombre
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * @param nombre the nombre to set
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * @return the ape1
+     */
     public String getApe1() {
         return ape1;
     }
 
+    /**
+     * @param ape1 the ape1 to set
+     */
     public void setApe1(String ape1) {
         this.ape1 = ape1;
     }
 
+    /**
+     * @return the ape2
+     */
     public String getApe2() {
         return ape2;
     }
 
+    /**
+     * @param ape2 the ape2 to set
+     */
     public void setApe2(String ape2) {
         this.ape2 = ape2;
     }
-
+ 
 }
